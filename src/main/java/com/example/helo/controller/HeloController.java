63d9db7bc357c85c980e9202359d455f70ce45a9
1в <cykalov.aleksand@mail.ru>
2025-07-28 22:54:44 +0300

    вариант согласно домашки

--- src/main/java/com/example/helo/controller/HeloController.java
+++ src/main/java/com/example/helo/controller/HeloController.java
@@ -1,12 +1,19 @@
 package com.example.helo.controller;
 
+import com.example.helo.HelloService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HeloController {
+    private final HelloService helloService;
+
+    public HeloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
     @GetMapping("/hello")
     public String hello(){
-        return "hello";
+        return helloService.hello();
     }
 }
---
+++ src/test/java/com/example/helo/HelloTest.java
@@ -0,0 +1,37 @@
+package com.example.helo;
+
+import com.example.helo.config.ProjectConfig;
+import com.example.helo.controller.HeloController;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@WebMvcTest(HeloController.class)
+//@Import(ProjectConfig.class)
+public class HelloTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockitoBean
+     HelloService helloService;
+    @InjectMocks
+    private HeloController heloController;
+    @Test
+        public void HeloControllerTest()throws Exception{
+      // when(helloService.hello()).thenReturn("Hello");
+       mockMvc.perform(MockMvcRequestBuilders.get("/hello")
+                        .accept(MediaType.APPLICATION_JSON))
+               .andDo(print())
+                .andExpect(status().is(401));
+    }
+
+}
